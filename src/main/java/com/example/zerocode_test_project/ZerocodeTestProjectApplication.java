569529ed1ca661937c6dad4b7bd1c955af9972fb
viduthranaweera2001 <viduthranaweera1234@gmail.com>
2024-03-12 13:53:57 +0530

    ZTP-3 first

---
+++ src/main/java/com/example/zerocode_test_project/ZerocodeTestProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.zerocode_test_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZerocodeTestProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZerocodeTestProjectApplication.class, args);
+    }
+
+}
